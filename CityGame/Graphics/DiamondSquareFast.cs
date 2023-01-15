ee315acc8246a978f6b940329e52dc41cee77eb4
Denys Kirin <dkirin@plusonex.com>
2023-01-14 20:34:21 -0800

    Fast Diamond completed

--- CityGame/Graphics/DiamondSquareFast.cs
+++ CityGame/Graphics/DiamondSquareFast.cs
@@ -28,7 +28,7 @@ namespace CityGame.Graphics
         {
             int x = x1 + halfSide;
             int y = y1 + halfSide;
-            t[x, y] = (t[x1, y1] + t[x2, y2] + t[x1, y2] + t[x2, y1] + rnd.Next(255)) / 5;
+            t[x, y] = (t[x1, y1] + t[x2, y2] + t[x1, y2] + t[x2, y1] + rnd.Next(512)) / 5;
         }
 
         private void SquareStep(int x1, int y1, int x2, int y2, int halfSide)
@@ -36,16 +36,16 @@ namespace CityGame.Graphics
             //sum of square
             int sum = (t[x1, y1] + t[x2, y2] + t[x1, y2] + t[x2, y1] + t[x1 + halfSide, y1 + halfSide]) / 5;
             //point 1
-            t[(x2 - x1) / 2, y1] = (sum + rnd.Next(255)) / 2;
+            t[(x2 - x1) / 2, y1] = (sum + rnd.Next(512)) / 2;
 
             //point 2
-            t[x2, (y2 - y1) / 2] = (sum + rnd.Next(255)) / 2;
+            t[x2, (y2 - y1) / 2] = (sum + rnd.Next(512)) / 2;
 
             //point 3
-            t[(x2 - x1) / 2, y2] = (sum + rnd.Next(255)) / 2;
+            t[(x2 - x1) / 2, y2] = (sum + rnd.Next(512)) / 2;
 
             //point 4
-            t[x1, (y2 - y1) / 2] = (sum + rnd.Next(255)) / 2;
+            t[x1, (y2 - y1) / 2] = (sum + rnd.Next(512)) / 2;
         }
 
         private void DeomandApStep(int x1, int y1, int x2, int y2, int halfSide)
@@ -77,10 +77,10 @@ namespace CityGame.Graphics
         {
             t = new int[terrainSize, terrainSize]; //"t" mean terrain
             //Init 
-            t[0, 0] = rnd.Next(255);
-            t[terrainSize - 1, 0] = rnd.Next(255);
-            t[terrainSize - 1, terrainSize - 1] = rnd.Next(255);
-            t[0, terrainSize - 1] = rnd.Next(255);
+            t[0, 0] = rnd.Next(512);
+            t[terrainSize - 1, 0] = rnd.Next(512);
+            t[terrainSize - 1, terrainSize - 1] = rnd.Next(512);
+            t[0, terrainSize - 1] = rnd.Next(512);
 
             //Steps            
             int sideLength = terrainSize - 1;
@@ -93,35 +93,33 @@ namespace CityGame.Graphics
                 {
                     for (int y = 0; y < terrainSize - 1; y += sideLength)
                     {
-                        try
-                        {
                             //DeomandStep(x, y, x + sideLength, y + sideLength, halfSide);
-                            t[x + halfSide, y + halfSide] = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + rnd.Next(255)) / 5;
+                            if ((x + halfSide < terrainSize) && (y + halfSide < terrainSize)
+                                && (x + sideLength < terrainSize) && (y + sideLength < terrainSize))
+                            {
+                                t[x + halfSide, y + halfSide] = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + rnd.Next(512)) / 5;
 
 
-                            //SquareStep(x, y, x + sideLength, y + sideLength, halfSide);
-                            //^^^this generate flat terrain
-                            //sum of square
+                                //SquareStep(x, y, x + sideLength, y + sideLength, halfSide);
+                                //^^^this generate flat terrain
+                                //sum of square
+
+                                //NOTE: this generate terrain with sea on righ buttom terrain side
+                                sum = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + t[x + halfSide, y + halfSide]) / 5;
+                                //point 1
+                                t[(x + sideLength) / 2, y] = (sum + rnd.Next(512)) / 2;
+
+                                //point 2
+                                t[x + sideLength, (y + sideLength) / 2] = (sum + rnd.Next(512)) / 2;
+
+                                //point 3
+                                t[(x + sideLength) / 2, y + sideLength] = (sum + rnd.Next(512)) / 2;
+
+                                //point 4
+                                t[x, (y + sideLength) / 2] = (sum + rnd.Next(512)) / 2;
+                            }
                             
-                            //NOTE: this generate terrain with sea on righ buttom terrain side
-                            sum = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + t[x + halfSide, y + halfSide]) / 5;
-                            //point 1
-                            t[(x + sideLength) /2, y] = (sum + rnd.Next(255)) / 2;
-
-                            //point 2
-                            t[x + sideLength, (y + sideLength) / 2] = (sum + rnd.Next(255)) / 2;
-
-                            //point 3
-                            t[(x + sideLength) / 2, y + sideLength] = (sum + rnd.Next(255)) / 2;
-
-                            //point 4
-                            t[x, (y + sideLength) / 2] = (sum + rnd.Next(255)) / 2;
-                            
-                        }
-                        catch
-                        {
-
-                        };
+                        
                     }
                 }
                 if (sideLength < 2)
@@ -131,6 +129,7 @@ namespace CityGame.Graphics
                 sideLength = (int)Math.Ceiling(sideLength / 2.0f);
             }
 
+            
             //smooth out roughness
             for (int r = 0; r < roughness; r++)
             {
@@ -143,15 +142,31 @@ namespace CityGame.Graphics
                     {
                         for (int y = 0; y < terrainSize - 1; y += sideLength)
                         {
-                            try
+                            //DeomandStep(x, y, x + sideLength, y + sideLength, halfSide);
+                            if ((x + halfSide < terrainSize) && (y + halfSide < terrainSize)
+                                && (x + sideLength < terrainSize) && (y + sideLength < terrainSize))
                             {
-                                DeomandApStep(x, y, x + sideLength, y + sideLength, halfSide);
-                                SquareApStep(x, y, x + sideLength, y + sideLength, halfSide);
-                            }
-                            catch
-                            {
+                                t[x + halfSide, y + halfSide] = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + t[x + halfSide, y + halfSide]) / 5;
 
-                            };
+
+                                //SquareStep(x, y, x + sideLength, y + sideLength, halfSide);
+                                //^^^this generate flat terrain
+                                //sum of square
+
+                                //NOTE: this generate terrain with sea on righ buttom terrain side
+                                sum = (t[x, y] + t[x + sideLength, y + sideLength] + t[x, y + sideLength] + t[x + sideLength, y] + t[x + halfSide, y + halfSide]) / 5;
+                                //point 1
+                                t[(x + sideLength) / 2, y] = (sum + t[(x + sideLength) / 2, y]) / 2;
+
+                                //point 2
+                                t[x + sideLength, (y + sideLength) / 2] = (sum + t[x + sideLength, (y + sideLength) / 2]) / 2;
+
+                                //point 3
+                                t[(x + sideLength) / 2, y + sideLength] = (sum + t[(x + sideLength) / 2, y + sideLength]) / 2;
+
+                                //point 4
+                                t[x, (y + sideLength) / 2] = (sum + t[x, (y + sideLength) / 2]) / 2;
+                            }
                         }
                     }
                     if (sideLength < 2)
@@ -161,6 +176,7 @@ namespace CityGame.Graphics
                     sideLength = (int)Math.Ceiling(sideLength / 2.0f);
                 }
             }
+            
             return t;
         }
 
--- CityGame/MainWindow.xaml.cs
+++ CityGame/MainWindow.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 
@@ -31,7 +30,18 @@ namespace CityGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Terrain width and height at blocks
+        /// Current block size is 16x16 piexels
+        /// The terrain size at pixels is terrainSize * 16
+        /// </summary>
         private const int terrainSize = 200;
+
+        /// <summary>
+        /// If true, the Diamond generator is not called, the terrain is simple random values 2D array 
+        /// The fast way of debuging all not related to terrain
+        /// </summary>
+        private const bool fackeDiomand = false; 
 
         //Terrain map
         //0 - water level
@@ -39,11 +49,11 @@ namespace CityGame
         //2 - forest
         private terrainType[,] terrain = new terrainType[terrainSize, terrainSize];
 
-        private int waterLevel = 80;
+        private int waterLevel = 120;
 
-        private int landLevel = 100; //upper this is forest        
+        private int landLevel = 130; //upper this is forest        
 
-        private int roughness = 0;
+        private int roughness = 4;
 
         private BlocksManager blocksManager = new BlocksManager();
 
@@ -53,6 +63,12 @@ namespace CityGame
 
         private DrawingVisual drawingVisual = new DrawingVisual();
 
+        private int scrollBorder = 100;
+
+        private double startScrollSpeed = 0.05f;
+
+        private bool lockScroll = false;
+        
 
         int count = 0;
         DateTime enter;
@@ -89,6 +105,124 @@ namespace CityGame
             DrawDiamand();
             BuildMap();
 
+            /*
+            var sb = new Storyboard();
+            var ta = new ThicknessAnimation();
+            ta.BeginTime = new TimeSpan(0);
+            ta.SetValue(Storyboard.TargetNameProperty, "TerrainImage");
+            Storyboard.SetTargetProperty(ta, new PropertyPath(MarginProperty));
+
+            ta.From = new Thickness(0, 30, 0, 0);
+            ta.To = new Thickness(-3000, 30, 0, 0);
+            ta.Duration = new Duration(TimeSpan.FromSeconds(30));
+
+            sb.Children.Add(ta);
+            sb.Begin(this);
+            */
+
+            // Create a TranslateTransform to
+            // move the rectangle.
+            /*
+            TranslateTransform animatedTranslateTransform =
+                new TranslateTransform();
+            TerrainImage.RenderTransform = animatedTranslateTransform;
+
+            // Assign the TranslateTransform a name so that
+            // it can be targeted by a Storyboard.
+            this.RegisterName(
+                "AnimatedTranslateTransform", animatedTranslateTransform);
+
+            // Create a DoubleAnimationUsingKeyFrames to
+            // animate the TranslateTransform.
+            DoubleAnimationUsingKeyFrames translationAnimation
+                = new DoubleAnimationUsingKeyFrames();
+            translationAnimation.Duration = TimeSpan.FromSeconds(26);
+
+            // Animate from the starting position to 500
+            // over the first second using linear
+            // interpolation.
+            translationAnimation.KeyFrames.Add(
+                new LinearDoubleKeyFrame(
+                    500, // Target value (KeyValue)
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(20))) // KeyTime
+                );
+
+            // Animate from 500 (the value of the previous key frame)
+            // to 400 at 4 seconds using discrete interpolation.
+            // Because the interpolation is discrete, the rectangle will appear
+            // to "jump" from 500 to 400.
+            translationAnimation.KeyFrames.Add(
+                new DiscreteDoubleKeyFrame(
+                    400, // Target value (KeyValue)
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(24))) // KeyTime
+                );
+
+            // Animate from 400 (the value of the previous key frame) to 0
+            // over two seconds, starting at 4 seconds (the key time of the
+            // last key frame) and ending at 6 seconds.
+            translationAnimation.KeyFrames.Add(
+                new SplineDoubleKeyFrame(
+                    0, // Target value (KeyValue)
+                    KeyTime.FromTimeSpan(TimeSpan.FromSeconds(6)), // KeyTime
+                    new KeySpline(0.6, 0.0, 0.9, 0.0) // KeySpline
+                    )
+                );
+
+            // Set the animation to repeat forever.
+            translationAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            // Set the animation to target the X property
+            // of the object named "AnimatedTranslateTransform."
+            Storyboard.SetTargetName(translationAnimation, "AnimatedTranslateTransform");
+            Storyboard.SetTargetProperty(
+                translationAnimation, new PropertyPath(TranslateTransform.XProperty));
+
+            // Create a storyboard to apply the animation.
+            Storyboard translationStoryboard = new Storyboard();
+            translationStoryboard.Children.Add(translationAnimation);
+
+            // Start the storyboard after the rectangle loads.
+            TerrainImage.Loaded += delegate (object sender, RoutedEventArgs e)
+            {
+                translationStoryboard.Begin(this);
+            };
+            */
+
+            /*
+
+            var tg = new TransformGroup();
+            var translation = new TranslateTransform(-300, 0);
+            var translationName = "myTranslation" + translation.GetHashCode();
+            RegisterName(translationName, translation);
+            tg.Children.Add(translation);
+            
+            TerrainImage.RenderTransform = tg;
+
+            //panel.Children.Add(e);
+
+            var anim = new DoubleAnimation(0, 1000, new Duration(TimeSpan.FromSeconds(20)))
+            {
+                EasingFunction = new PowerEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var s = new Storyboard();
+            Storyboard.SetTargetName(s, translationName);
+            Storyboard.SetTargetProperty(s, new PropertyPath(TranslateTransform.XProperty));
+            var storyboardName = "s" + s.GetHashCode();
+            Resources.Add(storyboardName, s);
+
+            s.Children.Add(anim);
+
+            /*
+            s.Completed +=
+                (sndr, evtArgs) => {
+                    panel.Children.Remove(e);
+                    Resources.Remove(storyboardName);
+                    UnregisterName(translationName);
+                };
+            
+            s.Begin();
+            */
 
         }
 
@@ -101,6 +235,8 @@ namespace CityGame
                 drawingContext.Close();
             }
             TerrainImage.Source = new DrawingImage(drawingVisual.Drawing);
+           // TerrainImage.Margin = new Thickness(TerrainImage.Margin.Left - 1, TerrainImage.Margin.Top, 0, 0);
+                       ((DispatcherTimer)sender).Stop();
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
@@ -108,9 +244,10 @@ namespace CityGame
 
             //random.NextBytes(pixels);
 
+            /*
             byte[] array = new byte[16 * 16];
 
-            /*
+            
             for (int i = 0; i < 5000; i++)
             {
                 Int32Rect rect = new Int32Rect(0, 0, 16, 16);
@@ -371,9 +508,23 @@ namespace CityGame
 
         private void DrawDiamand()
         {
-
-            DiamondSquareFast diamondSquare = new DiamondSquareFast(terrainSize, 1);
-            int[,] sourceTerraing = diamondSquare.getData();
+            int[,] sourceTerraing;
+            if (!fackeDiomand)
+            {
+                DiamondSquareFast diamondSquare = new DiamondSquareFast(terrainSize, roughness);
+                sourceTerraing = diamondSquare.getData();
+            }
+            else
+            {
+                sourceTerraing = new int[terrainSize, terrainSize];
+                for (int x = 0; x < terrainSize; x++)
+                {
+                    for (int y = 0; y < terrainSize; y++)
+                    {
+                        sourceTerraing[x, y] = random.Next(255);
+                    }
+                }
+            }
 
             Canvas.Children.Clear();
 
@@ -405,6 +556,7 @@ namespace CityGame
                     }
                     */
 
+                    /*
                     //Draw map
                     Rectangle rect = new Rectangle();
                     rect.Margin = new Thickness(x * 5, y * 5, 0, 0);
@@ -418,6 +570,7 @@ namespace CityGame
                         default: rect.Fill = new SolidColorBrush(Colors.Green); break;
                     }
                     Canvas.Children.Add(rect);
+                    */
                 }
             }
         }
@@ -503,45 +656,47 @@ namespace CityGame
 
                 TerrainImage.Width = TerrainImage.Height = terrainSize * ResourcesManager.iconsSizeInPixels * zoom;
             }
+
+            //lock scroll view            
+            e.Handled = true;
         }
 
         private void TerrainGrid_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            Point mousePosition = e.GetPosition((Grid)sender);
-
-            if (mousePosition.X > TerrainGrid.ActualWidth - 100)
+            if (!lockScroll)
             {
-                
-                {
-                    TerrainImage.Margin = new Thickness(TerrainImage.Margin.Left - 1, TerrainImage.Margin.Top, 0, 0);
-                }
-            }
-            else
-            if (mousePosition.X < 100)
-            {
-                
-                {
-                    TerrainImage.Margin = new Thickness(TerrainImage.Margin.Left + 1, TerrainImage.Margin.Top, 0, 0);
-                }
-            }
+                lockScroll= true;
 
-            if (mousePosition.Y > TerrainGrid.ActualHeight - 100)
-            {
-                
-                {
-                    TerrainImage.Margin = new Thickness(TerrainImage.Margin.Left, TerrainImage.Margin.Top - 1, 0, 0);
-                }
-            }
-            else
-            if (mousePosition.Y < 100)
-            {
-                
-                {
-                    TerrainImage.Margin = new Thickness(TerrainImage.Margin.Left, TerrainImage.Margin.Top + 1, 0, 0);
-                }
-            }
+                Point mousePosition = e.GetPosition((Grid)sender);
 
+                scrollBorder = (int)(TerrainGrid.ActualWidth + TerrainGrid.ActualHeight) / 20;
 
+                //prior is x
+                if (mousePosition.X > TerrainGrid.ActualWidth - scrollBorder)
+                {
+                        double xLambda = (scrollBorder / (TerrainGrid.ActualWidth - mousePosition.X)) * startScrollSpeed;                        
+                        TerrainScroll.ScrollToHorizontalOffset(TerrainScroll.HorizontalOffset + xLambda);
+                }
+                else
+                if (mousePosition.X < scrollBorder)
+                {
+                        double xLambda = (scrollBorder / (mousePosition.X)) * startScrollSpeed;
+                        TerrainScroll.ScrollToHorizontalOffset(TerrainScroll.HorizontalOffset - xLambda);
+                }
+
+                if (mousePosition.Y > TerrainGrid.ActualHeight - scrollBorder)
+                {
+                        double yLambda = (scrollBorder / (TerrainGrid.ActualHeight - mousePosition.Y)) * startScrollSpeed;
+                        TerrainScroll.ScrollToVerticalOffset(TerrainScroll.VerticalOffset + yLambda);
+                }
+                else
+                if (mousePosition.Y < scrollBorder)
+                {
+                    double yLambda = (scrollBorder / ( mousePosition.Y)) * startScrollSpeed;
+                    TerrainScroll.ScrollToVerticalOffset(TerrainScroll.VerticalOffset - yLambda);
+                }
+                lockScroll = false;
+            }
         }
     }
 }
